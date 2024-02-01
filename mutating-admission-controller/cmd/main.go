8de832aa0ae6faf67a5dd14d22424df02dffd5b9
joefurey2 <joefurey02@gmail.com>
2024-02-01 10:17:15 +0000

    Updated admission controller to use the go gin server framework instead of mux. Also added initial functionality to push and update ranking #12

--- mutating-admission-controller/cmd/main.go
+++ mutating-admission-controller/cmd/main.go
@@ -1,33 +1,92 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html"
 	"io"
 	"log"
 	"net/http"
 	"time"
+	"github.com/gin-gonic/gin"
 	mutate "github.com/joefurey2/mutating-admission-controller/pkg/mutate"
 )
 
+type Node struct {
+	Name string `json:"name"`
+	// Add other fields as needed
+}
 // Any path not specified be handled by this function
 // Prevents XSS attacks and other errors
-func handleRoot(w http.ResponseWriter, req *http.Request) {
+func handleRoot(c *gin.Context) {
 	log.Println("Root request!")
-	fmt.Fprint(w, "hello %q", html.EscapeString(req.URL.Path))
+	fmt.Fprint(c.Writer, "hello %q", html.EscapeString(c.Request.URL.Path))
 }
+
+
+var nodeList map[string]int
+
+// Update the ranking of nodes based on energy efficiency
+// This endpoint can be called to update the ranking of nodes
+func handleUpdate(c *gin.Context) {
+	body, err := io.ReadAll(c.Request.Body)
+	defer c.Request.Body.Close()
+
+	if err != nil {
+		log.Println(err)
+		c.Writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var nodes map[string]int
+	err = json.Unmarshal(body, &nodes)
+	if err != nil {
+		log.Println(err)
+		c.Writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = json.Unmarshal(body, &nodes)
+	log.Println(nodes)
+	if err != nil {
+		log.Println(err)
+		c.Writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	nodeList = nodes
+
+	log.Println("Node ranking updated!")
+	c.Writer.WriteHeader(http.StatusOK)
+}
+
+// Function to get current ranking of nodes stored in the system
+func handleGetRanking(c *gin.Context) {
+	rankingJSON, err := json.Marshal(nodeList)
+	if err != nil {
+		log.Println(err)
+		c.Writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.Writer.WriteHeader(http.StatusOK)
+	c.Writer.Write(rankingJSON)
+}
+
+
 
 /*
 	Admission controller recieves admission requests from the API server
 	It then reads and mutates the request, returning the mutated request to the API server
 */
-func handleMutate(w http.ResponseWriter, req *http.Request) {
-	body, err := io.ReadAll(req.Body)
-	defer req.Body.Close()
+func handleMutate(c *gin.Context) {
+	body, err := io.ReadAll(c.Request.Body)
+	defer c.Request.Body.Close()
 
 	if err != nil {
 		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
+		c.Writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
@@ -35,15 +94,15 @@ func handleMutate(w http.ResponseWriter, req *http.Request) {
 	mutated, err := mutate.MutateRequest(body)
 	if err != nil {
 		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
+		c.Writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}	
 	
 	log.Println("mutated!!")
 
 	// Send back mutated admission controller
-	w.WriteHeader(http.StatusOK)
-	w.Write(mutated)
+	c.Writer.WriteHeader(http.StatusOK)
+	c.Writer.Write(mutated)
 }
 
 
@@ -51,13 +110,15 @@ func main() {
 
 	log.Println("Starting server...")
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handleRoot)
-	mux.HandleFunc("/mutate", handleMutate)
+	router := gin.Default()
+	router.GET("/", handleRoot)
+	router.POST("/mutate", handleMutate)
+	router.POST("/ranking", handleUpdate)
+	router.GET("/ranking", handleGetRanking)
 
 	s := &http.Server {
 		Addr:           ":8443",
-		Handler: 	  	mux,
+		Handler: 	  	router,
 		ReadTimeout:   	10 * time.Second,
 		WriteTimeout: 	10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
