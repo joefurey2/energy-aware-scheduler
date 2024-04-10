ff19d05ac8d629459381419423cdcc0a6c86127d
joefurey2 <joefurey02@gmail.com>
2024-04-10 16:12:28 +0100

    fixed repeated combinations

--- profiling/scheduling/complete-profiling.py
+++ profiling/scheduling/complete-profiling.py
@@ -73,7 +73,7 @@ def runPods(v1, podTemplate, numInstances, nodes):
     allPodNames = {}
     for i in range(1, numInstances + 1):
         metrics[str(i) + 'instance'] = []
-        for combination in itertools.product(nodes, i):
+        for combination in itertools.combinations(nodes, i):
             nodeCounts = {node: combination.count(node) for node in nodes}
             combinationMetrics = {}
             counter = 1
@@ -85,7 +85,7 @@ def runPods(v1, podTemplate, numInstances, nodes):
                     podName = f"instances{i}-{combinationKey}-pod{counter}"
                     print(f"Creating pod {podName}...")
                     createPod(v1, podTemplate, podName, nodeName)
-                    counter += 1
+                    counter +=1
                     podNames.append(podName)
                 allPodNames[nodeName] = podNames
             for nodeName, podNames in allPodNames.items():
